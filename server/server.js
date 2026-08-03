f32e41c2bfeb2c1a77205c2751e3963c882d7416
Shreyansh Dixit <krishnadixit0808@gmail.com>
2026-08-03 22:21:35 +0530

    fix ai-processsing bug

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ dotenv.config();
 
 if (process.env.NODE_ENV === "production") {
   await import("./queue/worker.js");
+  await import("./queue/ai-worker.js");
 }
 import { initDB } from "./db.js";
 import { authMiddleware } from "./middleware/authMiddleware.js";
